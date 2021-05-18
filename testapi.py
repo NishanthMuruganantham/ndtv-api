4127883b83d7f420fdd00a5ec5bc558b70ec095c
NishanthMuruganantham <nishanthmurugananth10@gmail.com>
2021-05-19 00:14:34 +0530

    test file deleted

--- testapi.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-api_key = "e5bfda86c59d40298ecb5297067630e9"
-
-uri = "https://newsapi.org/v2/top-headlines?country=in&apiKey=e5bfda86c59d40298ecb5297067630e9"
-
-r = requests.get(uri)
-
-print(r.json())
