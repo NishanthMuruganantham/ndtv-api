ce34f6fdfbe2c19b6b5e8b4bad3972b8d4838c8e
NishanthMuruganantham <nishanthmurugananth10@gmail.com>
2021-05-19 12:07:00 +0530

    science app added

--- news.py
+++ news.py
@@ -3,17 +3,17 @@ from lxml import html,etree
 from flask import Flask
 from flask_restful import Resource,Api
 
+
 def fetch_news(domain_url):
-    #domain_url = "https://www.ndtv.com/world-news"
+    #by getting the last page in the pagination, we will come to know thw total number of pages for that category
     last_page_xpath = "//div[contains(@class,'listng_pagntn clear')]/a[contains(@class,'btnLnk arrowBtn next')]/preceding-sibling::a[position()=1]"
     page = requests.get(domain_url)
     tree = html.fromstring(page.content)
     total_pages = tree.xpath(last_page_xpath+"/text()")[0]
-
-    print(total_pages)
+    
     news_list = []
     
-    for page in range(1,int(total_pages)+1):
+    for page in range(1, int(total_pages) + 1):
         url = "{}/page-{}".format(domain_url,page)
         page = requests.get(url)
         tree = html.fromstring(page.content)
@@ -22,14 +22,12 @@ def fetch_news(domain_url):
         latest_news_xpath = tree.xpath(news_header_xpath)
         
         for i in range(0,len(latest_news_xpath)):
-            news_headline = tree.xpath(f"({news_header_xpath})[{i+1}]/text()")[0] #*
-            news_link = latest_news_xpath[i].get('href') #*
+            news_headline = tree.xpath(f"({news_header_xpath})[{i+1}]/text()")[0] #*headline
+            news_link = latest_news_xpath[i].get('href') #*url
             description_xpath = f"({news_header_xpath})[{i+1}]/parent::h2/following-sibling::p/text()"
-            description = tree.xpath(description_xpath)[0] #*
-            #print(description)
-            
+            description = tree.xpath(description_xpath)[0] #*description
             img_xpath = f"({news_header_xpath})[{i+1}]/parent::h2/parent::div/preceding-sibling::div/a/img"
-            img = tree.xpath(img_xpath)[0].get("src") #*
+            img = tree.xpath(img_xpath)[0].get("src") #*image_url
             
             news_list.append(
                 {
@@ -40,6 +38,7 @@ def fetch_news(domain_url):
                 }
             )
     return news_list
+
 
 class LatestNews(Resource):
     def get(self):
@@ -56,12 +55,18 @@ class IndiaNews(Resource):
         news_list = fetch_news(domain_url = "https://www.ndtv.com/india")
         return {"news_list" : news_list}
 
+class ScienceNews(Resource):
+    def get(self):
+        news_list = fetch_news(domain_url = "https://www.ndtv.com/science")
+        return {"news_list" : news_list}
+
 
 app = Flask(__name__)
 api = Api(app)
 api.add_resource(LatestNews, "/latest-news")
 api.add_resource(WorldNews, "/world-news")
 api.add_resource(IndiaNews, "/india-news")
+api.add_resource(ScienceNews, "/science-news")
 
 if __name__ == '__main__':
     app.run(debug=True)
