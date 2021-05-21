74f3aad1d743bfcc9f13642381efcba184e8f2d9
NishanthMuruganantham <nishanthmurugananth10@gmail.com>
2021-05-22 01:42:32 +0530

    reorganised the code, written the basics of giving only required values as response based on the request

--- NDTV.py
+++ NDTV.py
@@ -2,41 +2,49 @@ import requests
 import json
 from lxml import html
 from flask_restful import Resource
+import pandas as pd
+from flask import Flask, request
 
 
-def fetch_news(domain_url):
+
+def fetch_news(news_page_url):
     #by getting the last page in the pagination, we will come to know thw total number of pages for that category
     last_page_xpath = "//div[contains(@class,'listng_pagntn clear')]/a[contains(@class,'btnLnk arrowBtn next')]/preceding-sibling::a[position()=1]"
-    page = requests.get(domain_url)
+    page = requests.get(news_page_url)
     tree = html.fromstring(page.content)
     total_pages = tree.xpath(last_page_xpath+"/text()")[0]
     
     news_list = []
     
     for page in range(1, int(total_pages) + 1):
-        url = "{}/page-{}".format(domain_url,page)
-        page = requests.get(url)
+        page_url = f"{news_page_url}/page-{page}"
+        page = requests.get(page_url)
         tree = html.fromstring(page.content)
         news_header_xpath = "//h2[contains(@class,'newsHdng')]/a"
         
-        latest_news_xpath = tree.xpath(news_header_xpath)
+        headline_elements = tree.xpath(news_header_xpath)
         
-        for i in range(0,len(latest_news_xpath)):
-            news_headline = tree.xpath(f"({news_header_xpath})[{i+1}]/text()")[0] #*headline
-            news_link = latest_news_xpath[i].get('href') #*url
-            description_xpath = f"({news_header_xpath})[{i+1}]/parent::h2/following-sibling::p/text()"
+        for i in range(1,int(len(headline_elements)) + 1):
+            news_headline = tree.xpath(f"({news_header_xpath})[{i}]/text()")[0] #*headline
+            news_url = headline_elements[i-1].get('href') #*url
+            description_xpath = f"({news_header_xpath})[{i}]/parent::h2/following-sibling::p/text()"
             description = tree.xpath(description_xpath)[0] #*description
-            img_xpath = f"({news_header_xpath})[{i+1}]/parent::h2/parent::div/preceding-sibling::div/a/img"
-            img = tree.xpath(img_xpath)[0].get("src") #*image_url
+            img_xpath = f"({news_header_xpath})[{i}]/parent::h2/parent::div/preceding-sibling::div/a/img"
+            img_url = tree.xpath(img_xpath)[0].get("src") #*image_url
             
-            news_list.append(
-                {
+            news_dict = {
                     "headline" : news_headline,
-                    "url" : news_link,
+                    "url" : news_url,
                     "description" : description,
-                    "image_url" : img
+                    "image_url" : img_url
                 }
+            
+            res = {key: news_dict[key] for key in news_dict.keys()
+                               & {'headline', 'url'}}
+            news_list.append(
+                res
             )
+    
     return news_list
 
 
@@ -71,32 +79,34 @@ def fetch_sports_news(url):
 
 class LatestNews(Resource):
     def get(self):
-        news_list = fetch_news(domain_url = "https://www.ndtv.com/latest")
+        a = request.args.get("field")
+        print(type(a))
+        news_list = fetch_news(news_page_url = "https://www.ndtv.com/latest")
         return {"news_list" : news_list}
 
 class WorldNews(Resource):
     def get(self):
-        news_list = fetch_news(domain_url = "https://www.ndtv.com/world-news")
+        news_list = fetch_news(news_page_url = "https://www.ndtv.com/world-news")
         return {"news_list" : news_list}
 
 class IndiaNews(Resource):
     def get(self):
-        news_list = fetch_news(domain_url = "https://www.ndtv.com/india")
+        news_list = fetch_news(news_page_url = "https://www.ndtv.com/india")
         return {"news_list" : news_list}
 
 class ScienceNews(Resource):
     def get(self):
-        news_list = fetch_news(domain_url = "https://www.ndtv.com/science")
+        news_list = fetch_news(news_page_url = "https://www.ndtv.com/science")
         return {"news_list" : news_list}
 
 class BusinessNews(Resource):
     def get(self):
-        news_list = fetch_news(domain_url = "https://www.ndtv.com/business/latest")
+        news_list = fetch_news(news_page_url = "https://www.ndtv.com/business/latest")
         return {"news_list" : news_list}
 
 class EntertainmentNews(Resource):
     def get(self):
-        news_list = fetch_news(domain_url = "https://www.ndtv.com/entertainment/latest")
+        news_list = fetch_news(news_page_url = "https://www.ndtv.com/entertainment/latest")
         return {"news_list" : news_list}
 
 class GeneralNews(Resource):
@@ -104,7 +114,7 @@ class GeneralNews(Resource):
         self.url = url
     
     def get(self):
-        news_list = fetch_news(domain_url = self.url)
+        news_list = fetch_news(news_page_url = self.url)
         return {"news_list" : news_list}
 
 class SportsNews(Resource):
